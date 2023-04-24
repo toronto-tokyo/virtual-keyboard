66887cf4902a2cd0c471793e32d51ee7df930839
Minchuk Ilya <cacatubutyt@gmail.com>
2023-04-24 17:11:48 +0200

    refactor: add missing semi-colons, commas and change ticks in webpack.config.js file according to eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,51 @@
-const path = require('path')
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const mode = process.env.NODE_ENV || 'development'
-const devMode = mode === 'development'
-const target = devMode ? 'web' : 'browserslist'
-const devtool = devMode ? 'source-map' : undefined
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
   mode,
   target,
   devtool,
   devServer: {
-    open: true
+    open: true,
   },
   entry: path.resolve(__dirname, 'src', 'index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
-    filename: 'index.[contenthash].js'
+    filename: 'index.[contenthash].js',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
     new MiniCssExtractPlugin({
-      filename: 'style.[contenthash].css'
+      filename: 'style.[contenthash].css',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.css$/i,
         use: [
-          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-          "css-loader"],
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader'],
       },
       {
         test: /\.woff2?$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name][ext]'
-        }
+          filename: 'fonts/[name][ext]',
+        },
       },
       {
         test: /\.(?:js|mjs|cjs)$/i,
@@ -53,12 +54,12 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: [
-              ['@babel/preset-env', { targets: "defaults" }]
+              ['@babel/preset-env', { targets: 'defaults' }],
             ],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
-      }
+            plugins: ['@babel/plugin-proposal-class-properties'],
+          },
+        },
+      },
     ],
   },
-}
+};
