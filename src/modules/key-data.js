5f4a6c64106ec0c53211bd3c253286181bdb3e42
Minchuk Ilya <cacatubutyt@gmail.com>
2023-04-29 23:49:19 +0200

    refactor: add keys data in english

---
+++ src/modules/key-data.js
@@ -0,0 +1,645 @@
+const KEYS_DATA = {
+  Backquote: {
+    en: {
+      lowerCase: '`',
+      caps: '`',
+      shift: '~',
+      capsShift: '~',
+    },
+    ru: {
+    },
+  },
+  Digit1: {
+    en: {
+      lowerCase: '1',
+      caps: '1',
+      shift: '!',
+      capsShift: '!',
+    },
+    ru: {
+    },
+  },
+  Digit2: {
+    en: {
+      lowerCase: '2',
+      caps: '2',
+      shift: '@',
+      capsShift: '@',
+    },
+    ru: {
+    },
+  },
+  Digit3: {
+    en: {
+      lowerCase: '3`',
+      caps: '3',
+      shift: '#',
+      capsShift: '#',
+    },
+    ru: {
+    },
+  },
+  Digit4: {
+    en: {
+      lowerCase: '4',
+      caps: '4',
+      shift: '$',
+      capsShift: '$',
+    },
+    ru: {
+    },
+  },
+  Digit5: {
+    en: {
+      lowerCase: '5',
+      caps: '5',
+      shift: '%',
+      capsShift: '%',
+    },
+    ru: {
+    },
+  },
+  Digit6: {
+    en: {
+      lowerCase: '6',
+      caps: '6',
+      shift: '^',
+      capsShift: '^',
+    },
+    ru: {
+    },
+  },
+  Digit7: {
+    en: {
+      lowerCase: '7',
+      caps: '7',
+      shift: '&',
+      capsShift: '&',
+    },
+    ru: {
+    },
+  },
+  Digit8: {
+    en: {
+      lowerCase: '8',
+      caps: '8',
+      shift: '*',
+      capsShift: '*',
+    },
+    ru: {
+    },
+  },
+  Digit9: {
+    en: {
+      lowerCase: '9',
+      caps: '9',
+      shift: '(',
+      capsShift: '(',
+    },
+    ru: {
+    },
+  },
+  Digit0: {
+    en: {
+      lowerCase: '0',
+      caps: '0',
+      shift: ')',
+      capsShift: ')',
+    },
+    ru: {
+    },
+  },
+  Minus: {
+    en: {
+      lowerCase: '-',
+      caps: '-',
+      shift: '_',
+      capsShift: '_',
+    },
+    ru: {
+    },
+  },
+  Equal: {
+    en: {
+      lowerCase: '=',
+      caps: '=',
+      shift: '+',
+      capsShift: '+',
+    },
+    ru: {
+    },
+  },
+  Backspace: {
+    en: {
+      lowerCase: 'Backspace',
+      caps: 'Backspace',
+      shift: 'Backspace',
+      capsShift: 'Backspace',
+    },
+    ru: {
+    },
+  },
+  Tab: {
+    en: {
+      lowerCase: 'Tab',
+      caps: 'Tab',
+      shift: 'Tab',
+      capsShift: 'Tab',
+    },
+    ru: {
+    },
+  },
+  KeyQ: {
+    en: {
+      lowerCase: 'q',
+      caps: 'Q',
+      shift: 'Q',
+      capsShift: 'q',
+    },
+    ru: {
+    },
+  },
+  KeyW: {
+    en: {
+      lowerCase: 'w',
+      caps: 'W',
+      shift: 'W',
+      capsShift: 'w',
+    },
+    ru: {
+    },
+  },
+  KeyE: {
+    en: {
+      lowerCase: 'e',
+      caps: 'E',
+      shift: 'E',
+      capsShift: 'e',
+    },
+    ru: {
+    },
+  },
+  KeyR: {
+    en: {
+      lowerCase: 'r',
+      caps: 'R',
+      shift: 'R',
+      capsShift: 'r',
+    },
+    ru: {
+    },
+  },
+  KeyT: {
+    en: {
+      lowerCase: 't',
+      caps: 'T',
+      shift: 'T',
+      capsShift: 't',
+    },
+    ru: {
+    },
+  },
+  KeyY: {
+    en: {
+      lowerCase: 'y',
+      caps: 'Y',
+      shift: 'Y',
+      capsShift: 'y',
+    },
+    ru: {
+    },
+  },
+  KeyU: {
+    en: {
+      lowerCase: 'u',
+      caps: 'U',
+      shift: 'U',
+      capsShift: 'u',
+    },
+    ru: {
+    },
+  },
+  KeyI: {
+    en: {
+      lowerCase: 'i',
+      caps: 'I',
+      shift: 'I',
+      capsShift: 'i',
+    },
+    ru: {
+    },
+  },
+  KeyO: {
+    en: {
+      lowerCase: 'o',
+      caps: 'O',
+      shift: 'O',
+      capsShift: 'o',
+    },
+    ru: {
+    },
+  },
+  KeyP: {
+    en: {
+      lowerCase: 'p',
+      caps: 'P',
+      shift: 'P',
+      capsShift: 'p',
+    },
+    ru: {
+    },
+  },
+  BracketLeft: {
+    en: {
+      lowerCase: '[',
+      caps: '[',
+      shift: '{',
+      capsShift: '{',
+    },
+    ru: {
+    },
+  },
+  BracketRight: {
+    en: {
+      lowerCase: ']',
+      caps: ']',
+      shift: '}',
+      capsShift: '}',
+    },
+    ru: {
+    },
+  },
+  Backslash: {
+    en: {
+      lowerCase: '\\',
+      caps: '\\',
+      shift: '|',
+      capsShift: '|',
+    },
+    ru: {
+    },
+  },
+  Delete: {
+    en: {
+      lowerCase: 'Delete',
+      caps: 'Delete',
+      shift: 'Delete',
+      capsShift: 'Delete',
+    },
+    ru: {
+      shiftActive: '!',
+    },
+  },
+  CapsLock: {
+    en: {
+      lowerCase: 'Caps Lock',
+      caps: 'Caps Lock',
+      shift: 'Caps Lock',
+      capsShift: 'Caps Lock',
+    },
+    ru: {
+    },
+  },
+  KeyA: {
+    en: {
+      lowerCase: 'a',
+      caps: 'A',
+      shift: 'A',
+      capsShift: 'a',
+    },
+    ru: {
+    },
+  },
+  KeyS: {
+    en: {
+      lowerCase: 's',
+      caps: 'S',
+      shift: 'S',
+      capsShift: 's',
+    },
+    ru: {
+    },
+  },
+  KeyD: {
+    en: {
+      lowerCase: 'd',
+      caps: 'D',
+      shift: 'D',
+      capsShift: 'd',
+    },
+    ru: {
+    },
+  },
+  KeyF: {
+    en: {
+      lowerCase: 'f',
+      caps: 'F',
+      shift: 'F',
+      capsShift: 'f',
+    },
+    ru: {
+    },
+  },
+  KeyG: {
+    en: {
+      lowerCase: 'g',
+      caps: 'G',
+      shift: 'G',
+      capsShift: 'g',
+    },
+    ru: {
+    },
+  },
+  KeyH: {
+    en: {
+      lowerCase: 'h',
+      caps: 'H',
+      shift: 'H',
+      capsShift: 'h',
+    },
+    ru: {
+    },
+  },
+  KeyJ: {
+    en: {
+      lowerCase: 'j',
+      caps: 'J',
+      shift: 'J',
+      capsShift: 'j',
+    },
+    ru: {
+    },
+  },
+  KeyK: {
+    en: {
+      lowerCase: 'k',
+      caps: 'K',
+      shift: 'K',
+      capsShift: 'k',
+    },
+    ru: {
+    },
+  },
+  KeyL: {
+    en: {
+      lowerCase: 'l',
+      caps: 'L',
+      shift: 'L',
+      capsShift: 'l',
+    },
+    ru: {
+    },
+  },
+  Semicolon: {
+    en: {
+      lowerCase: ';',
+      caps: ';',
+      shift: ':',
+      capsShift: ':',
+    },
+    ru: {
+    },
+  },
+  Quote: {
+    en: {
+      lowerCase: '\'',
+      caps: '\'',
+      shift: '"',
+      capsShift: '"',
+    },
+    ru: {
+    },
+  },
+  Enter: {
+    en: {
+      lowerCase: 'Enter',
+      caps: 'Enter',
+      shift: 'Enter',
+      capsShift: 'Enter',
+    },
+    ru: {
+    },
+  },
+  ShiftLeft: {
+    en: {
+      lowerCase: 'Shift',
+      caps: 'Shift',
+      shift: 'Shift',
+      capsShift: 'Shift',
+    },
+    ru: {
+    },
+  },
+  KeyZ: {
+    en: {
+      lowerCase: 'z',
+      caps: 'Z',
+      shift: 'Z',
+      capsShift: 'z',
+    },
+    ru: {
+    },
+  },
+  KeyX: {
+    en: {
+      lowerCase: 'x',
+      caps: 'X',
+      shift: 'X',
+      capsShift: 'x',
+    },
+    ru: {
+    },
+  },
+  KeyC: {
+    en: {
+      lowerCase: 'c',
+      caps: 'C',
+      shift: 'C',
+      capsShift: 'c',
+    },
+    ru: {
+    },
+  },
+  KeyV: {
+    en: {
+      lowerCase: 'v',
+      caps: 'V',
+      shift: 'V',
+      capsShift: 'v',
+    },
+    ru: {
+    },
+  },
+  KeyB: {
+    en: {
+      lowerCase: 'b',
+      caps: 'B',
+      shift: 'B',
+      capsShift: 'b',
+    },
+    ru: {
+    },
+  },
+  KeyN: {
+    en: {
+      lowerCase: 'n',
+      caps: 'N',
+      shift: 'N',
+      capsShift: 'n',
+    },
+    ru: {
+    },
+  },
+  KeyM: {
+    en: {
+      lowerCase: 'm',
+      caps: 'M',
+      shift: 'M',
+      capsShift: 'm',
+    },
+    ru: {
+    },
+  },
+  Comma: {
+    en: {
+      lowerCase: ',',
+      caps: ',',
+      shift: '<',
+      capsShift: '<',
+    },
+    ru: {
+    },
+  },
+  Period: {
+    en: {
+      lowerCase: '.',
+      caps: '.',
+      shift: '>',
+      capsShift: '>',
+    },
+    ru: {
+    },
+  },
+  Slash: {
+    en: {
+      lowerCase: '/',
+      caps: '/',
+      shift: '?',
+      capsShift: '?',
+    },
+    ru: {
+    },
+  },
+  ArrowUp: {
+    en: {
+      lowerCase: '&#x25B2;',
+      caps: '&#x25B2;',
+      shift: '&#x25B2;',
+      capsShift: '&#x25B2;',
+    },
+    ru: {
+    },
+  },
+  ShiftRight: {
+    en: {
+      lowerCase: 'Shift',
+      caps: 'Shift',
+      shift: 'Shift',
+      capsShift: 'Shift',
+    },
+    ru: {
+    },
+  },
+  ControlLeft: {
+    en: {
+      lowerCase: 'Ctrl',
+      caps: 'Ctrl',
+      shift: 'Ctrl',
+      capsShift: 'Ctrl',
+    },
+    ru: {
+    },
+  },
+  MetaLeft: {
+    en: {
+      lowerCase: 'Win',
+      caps: 'Win',
+      shift: 'Win',
+      capsShift: 'Win',
+    },
+    ru: {
+    },
+  },
+  AltLeft: {
+    en: {
+      lowerCase: 'Alt',
+      caps: 'Alt',
+      shift: 'Alt',
+      capsShift: 'Alt',
+    },
+    ru: {
+    },
+  },
+  Space: {
+    en: {
+      lowerCase: '',
+      caps: '',
+      shift: '',
+      capsShift: '',
+    },
+    ru: {
+    },
+  },
+  AltRight: {
+    en: {
+      lowerCase: 'Alt',
+      caps: 'Alt',
+      shift: 'Alt',
+      capsShift: 'Alt',
+    },
+    ru: {
+    },
+  },
+  ArrowLeft: {
+    en: {
+      lowerCase: '&#x25C4;',
+      caps: '&#x25C4;',
+      shift: '&#x25C4;',
+      capsShift: '&#x25C4;',
+    },
+    ru: {
+    },
+  },
+  ArrowDown: {
+    en: {
+      lowerCase: '&#x25BC;',
+      caps: '&#x25BC;',
+      shift: '&#x25BC;',
+      capsShift: '&#x25BC;',
+    },
+    ru: {
+    },
+  },
+  ArrowRight: {
+    en: {
+      lowerCase: '&#x25BA;',
+      caps: '&#x25BA;',
+      shift: '&#x25BA;',
+      capsShift: '&#x25BA;',
+    },
+    ru: {
+    },
+  },
+  ControlRight: {
+    en: {
+      lowerCase: 'Ctrl',
+      caps: 'Ctrl',
+      shift: 'Ctrl',
+      capsShift: 'Ctrl',
+    },
+    ru: {
+    },
+  },
+};
+
+export default KEYS_DATA;
