0b2231d2bafe93298e3b61846351677ca5832d96
Minchuk Ilya <cacatubutyt@gmail.com>
2023-04-30 23:35:26 +0200

    feat: add css-classes for special keys, change textContent to innerHTML for arrow keys to add mnenomic symbols

--- src/modules/Key.js
+++ src/modules/Key.js
@@ -1,8 +1,17 @@
+import SPECIAL_KEYS from './special-keys-data';
+
 class Key {
   constructor(container, keyData) {
     this.container = container;
     this.element = document.createElement('div');
     this.element.className = 'key';
+    if (SPECIAL_KEYS.nonSymbolKeys.includes(keyData.code)) {
+      this.element.classList.add(`${keyData.code.toLowerCase()}`);
+      this.element.classList.add('key--special');
+    }
+    if (SPECIAL_KEYS.arrowKeys.includes(keyData.code)) {
+      this.element.classList.add('key--special');
+    }
     this.keyEnContentWrapper = document.createElement('span');
     this.keyEnContentWrapper.className = 'key__en';
     this.keyRuContentWrapper = document.createElement('span');
@@ -23,7 +32,7 @@ class Key {
           const keyChar = document.createElement('span');
           keyChar.className = 'key__char';
           if (i !== 0) keyChar.classList.add('hidden');
-          if (keyData.code === 'ArrowUp' || keyData.code === 'ArrowLeft' || keyData.code === 'ArrowDown' || keyData.code === 'ArrowRight') {
+          if (SPECIAL_KEYS.arrowKeys.includes(keyData.code)) {
             keyChar.innerHTML = keyData.en[objectKeys[i]];
           } else {
             keyChar.textContent = keyData.en[objectKeys[i]];
@@ -35,7 +44,11 @@ class Key {
         const objectKeys = Object.keys(keyData.ru);
         for (let i = 0; i < objectKeys.length; i += 1) {
           const keyChar = document.createElement('span');
-          keyChar.textContent = keyData.ru[objectKeys[i]];
+          if (SPECIAL_KEYS.arrowKeys.includes(keyData.code)) {
+            keyChar.innerHTML = keyData.ru[objectKeys[i]];
+          } else {
+            keyChar.textContent = keyData.ru[objectKeys[i]];
+          }
           this.keyRuContentWrapper.append(keyChar);
         }
       }
