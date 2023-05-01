3829177a326042d57fcbc0cff7c5c13b1152f75c
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-01 13:22:55 +0200

    feat: add addition information about system and switch language keys

--- src/modules/Layout.js
+++ src/modules/Layout.js
@@ -14,9 +14,20 @@ class Layout {
   }
 
   createElements() {
+    this.additionInfo = document.createElement('div');
+    this.additionInfo.className = 'addition-info';
+    this.additionInfoSystem = document.createElement('p');
+    this.additionInfoSwitchLang = document.createElement('p');
+    this.additionInfoSystem.className = 'addition-info__content';
+    this.additionInfoSwitchLang.className = 'addition-info__content';
+    this.additionInfoSystem.textContent = 'Клавиатура создана в операционной системе Windows';
+    this.additionInfoSwitchLang.textContent = 'Для переключения языка комбинация: левыe ctrl + alt';
+    this.additionInfo.append(this.additionInfoSystem);
+    this.additionInfo.append(this.additionInfoSwitchLang);
     this.title = new Title(this.container, 'h1', 'RSS Виртуальная клавиатура');
     this.screen = new Screen(this.container);
     this.keyboard = new Keyboard(this.container);
+    this.container.append(this.additionInfo);
     this.keyboard.addKeys();
     this.keyboard.pressKeys();
   }
