90835f963bb4c11840b51971085d929c431f72ef
Minchuk Ilya <cacatubutyt@gmail.com>
2023-05-01 10:43:29 +0200

    fix: fix eslint mistake with pressKeys method at Keyboard class with no this in method

--- src/modules/Keyboard.js
+++ src/modules/Keyboard.js
@@ -35,16 +35,16 @@ class Keyboard {
   }
 
   pressKeys() {
-    const keyElements = Array.from(document.querySelectorAll('.key'));
+    this.keyElements = Array.from(document.querySelectorAll('.key'));
     document.addEventListener('keydown', (e) => {
       if (e.altKey || e.code === 'Tab') e.preventDefault();
       const eCode = e.code;
-      const element = keyElements.find((el) => el.dataset.code === eCode);
+      const element = this.keyElements.find((el) => el.dataset.code === eCode);
       element?.classList.add('active');
     });
     document.addEventListener('keyup', (e) => {
       const eCode = e.code;
-      const element = keyElements.find((el) => el.dataset.code === eCode);
+      const element = this.keyElements.find((el) => el.dataset.code === eCode);
       element?.classList.remove('active');
     });
   }
